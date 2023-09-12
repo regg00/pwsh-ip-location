0ab8df0785d432e634a915778c31e4c9a65237ef
Régis Tremblay Lefrançois <regg00@gmail.com>
2023-09-12 07:30:47 -0400

    fixed issues

--- Helpers/CodeCoverage.ps1
+++ Helpers/CodeCoverage.ps1
@@ -1,2 +1,1 @@
-#Requires -RunAsAdministrator
 Invoke-Pester -CodeCoverage Public\*
--- Helpers/ScriptAnalyzer.ps1
+++ Helpers/ScriptAnalyzer.ps1
@@ -1,5 +1,4 @@
 $RootPath = Split-Path $PSScriptRoot -Parent
 
-Invoke-ScriptAnalyzer -Recurse (Join-Path $RootPath "Private")
 Invoke-ScriptAnalyzer -Recurse (Join-Path $RootPath "Public")
 Invoke-ScriptAnalyzer -Recurse (Join-Path $RootPath "Helpers")
